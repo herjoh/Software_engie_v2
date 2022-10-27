481c3cdc5802ca19a2a292cd3b90ffa2b4ee22a0
herjoh <73656823+herjoh@users.noreply.github.com>
2022-10-27 19:44:57 +0200

    Update tests.py

--- tests.py
+++ tests.py
@@ -11,11 +11,7 @@ def test_Acceptance_criteria_1_leap_year(input_value):
 def test_Acceptance_criteria_2_leap_year(input_value):
     assert input_value % 400 == 0
 
-def test_Acceptance_criteria_1__for_not_leap_year(input_value):
-    assert input_value % 4 != 0
 
-def test_Acceptance_criteria_2__for_not_leap_year(input_value):
-    assert input_value % 100 != 0 and not input_value % 400 == 0
 
 
 
